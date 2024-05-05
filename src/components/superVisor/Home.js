ef7439aed4bb4f91f840febdac15a63a3090af5a
rootMa99 <anasszeroual09@gmail.com>
2024-05-05 12:18:13 +0100

    fp

---
+++ src/components/superVisor/Home.js
@@ -0,0 +1,21 @@
+
+import c from './Home.module.css';
+
+const Home=p=>{
+
+
+    const deletAudit=e=>{
+
+    }
+    const planifyAudit=p=>[
+        
+    ]
+
+    return(
+        <div className={c.container} >
+
+        </div>
+    )
+
+}
+export default Home;
