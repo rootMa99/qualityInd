6699457de9b1f3053150eeacd5ba10130b9a137b
anass zeroual <anasszeroual09@gmail.com>
2024-05-06 10:57:28 +0100

    zety

--- src/DemoData.js
+++ src/DemoData.js
@@ -1,14 +1,211 @@
 export const USERS = [
-  { role: "audit", userName: "aux", token: "xxx.yyy.zzz"},
+  { role: "audit", userName: "aux", token: "xxx.yyy.zzz" },
   { role: "sp", userName: "spx", token: "xxx.yyy.zzz" },
 ];
 
-
-export const AUDITOR=[
-    {matricule:"1", fullName:"x"},
-    {matricule:"12", fullName:"xy"},
-    {matricule:"123", fullName:"xx"},
-    {matricule:"1234", fullName:"xs"},
-    {matricule:"12345", fullName:"xop"},
-    {matricule:"123456", fullName:"xkh"},
-]
+export const AUDITOR = [
+  {
+    matricule: 41,
+    fullName: "KHEDOUD KHALID",
+  },
+  {
+    matricule: 257,
+    fullName: "OUBA SAID",
+  },
+  {
+    matricule: 282,
+    fullName: "TAHTAHA  MOHAMMED",
+  },
+  {
+    matricule: 285,
+    fullName: "MEZIANE HAMID",
+  },
+  {
+    matricule: 319,
+    fullName: "AIT M'BAREK JAMAL",
+  },
+  {
+    matricule: 360,
+    fullName: "ZERROUK  RACHIDA",
+  },
+  {
+    matricule: 361,
+    fullName: "EL-BALDI  ISMAIL",
+  },
+  {
+    matricule: 363,
+    fullName: "Haoudar  Hamid",
+  },
+  {
+    matricule: 366,
+    fullName: "CHUIAF YOUSSEF",
+  },
+  {
+    matricule: 367,
+    fullName: " ACHBANI  MOHAMED",
+  },
+  {
+    matricule: 385,
+    fullName: "JNYAH ABDELILAH",
+  },
+  {
+    matricule: 400,
+    fullName: "ABIDA LAILA",
+  },
+  {
+    matricule: 410,
+    fullName: "KENNOUF   SAIDA    ",
+  },
+  {
+    matricule: 420,
+    fullName: "AHMED ECHRIF  HICHAM      ",
+  },
+  {
+    matricule: 437,
+    fullName: "JAIT   MOHAMED      ",
+  },
+  {
+    matricule: 454,
+    fullName: "EL KASMI ABDELILAH",
+  },
+  {
+    matricule: 486,
+    fullName: "LAHMAM  ABDELLAH         ",
+  },
+  {
+    matricule: 502,
+    fullName: "BOUZIANI RACHID",
+  },
+  {
+    matricule: 518,
+    fullName: "LAOUHIDI  ASMAE",
+  },
+  {
+    matricule: 519,
+    fullName: "KHABIR ABDELKADER",
+  },
+  {
+    matricule: 520,
+    fullName: "EL SGHIOUER YOUNOUSS",
+  },
+  {
+    matricule: 522,
+    fullName: "BOUJADDAYNE AZZEDDINE",
+  },
+  {
+    matricule: 533,
+    fullName: "BAJJIOUI RAJAE",
+  },
+  {
+    matricule: 534,
+    fullName: "EL AGOURI MOURAD",
+  },
+  {
+    matricule: 562,
+    fullName: "EL KHACHANY MOHAMED",
+  },
+  {
+    matricule: 667,
+    fullName: "MESKI ANASS",
+  },
+  {
+    matricule: 724,
+    fullName: "AAGOUN NOURDDINE",
+  },
+  {
+    matricule: 725,
+    fullName: "BENHAMMI MUSTAPHA",
+  },
+  {
+    matricule: 748,
+    fullName: "BELMALEM MARWANE",
+  },
+  {
+    matricule: 754,
+    fullName: "BOUTBAK EL MAHDI",
+  },
+  {
+    matricule: 768,
+    fullName: "MAHBOUB KHADIJA",
+  },
+  {
+    matricule: 785,
+    fullName: "MARMOUCHE ASMAE",
+  },
+  {
+    matricule: 882,
+    fullName: "BENNYS FAICAL",
+  },
+  {
+    matricule: 841,
+    fullName: "BOUKALLAL  OTHMANE",
+  },
+  {
+    matricule: 953,
+    fullName: "BENMESSAOUD AHMED",
+  },
+  {
+    matricule: 961,
+    fullName: "LAZZAR ZOUHAYR",
+  },
+  {
+    matricule: 1035,
+    fullName: "ES- SAYYAF MOHAMED",
+  },
+  {
+    matricule: 1097,
+    fullName: "ZAHI AHMED",
+  },
+  {
+    matricule: 1147,
+    fullName: "MAHTAT FATIHA",
+  },
+  {
+    matricule: 1150,
+    fullName: "EL BANANE SAID",
+  },
+  {
+    matricule: 1249,
+    fullName: "IDRISSI OTMANE",
+  },
+  {
+    matricule: 1361,
+    fullName: "KAMEL NABIL",
+  },
+  {
+    matricule: 1410,
+    fullName: "Slami Zouhir",
+  },
+  {
+    matricule: 1545,
+    fullName: "ATTARI ACHRAF",
+  },
+  {
+    matricule: 1678,
+    fullName: "EL HAOUARI  MOHAMED",
+  },
+  {
+    matricule: 3173,
+    fullName: "STITOU AISSAM",
+  },
+  {
+    matricule: 4695,
+    fullName: "SGHIR RACHID",
+  },
+  {
+    matricule: 15,
+    fullName: "BAHMANE AHMED",
+  },
+  {
+    matricule: 933,
+    fullName: "NOURI AHMED",
+  },
+  {
+    matricule: 1253,
+    fullName: "FARAJI ISMAIL",
+  },
+  {
+    matricule: 3070,
+    fullName: "HAZGAINE SAID",
+  },
+];
