d4cc05e38c0c1cf37f06513c7a9cb45c3c4fd8a9
anass zeroual <anasszeroual09@gmail.com>
2024-05-08 09:38:44 +0100

    fatmizzo

--- src/components/auditor/Home.js
+++ src/components/auditor/Home.js
@@ -6,6 +6,7 @@ import { getCurrentWeek } from "../hooks/hfunc";
 const Home = (p) => {
   const { isLoged } = useSelector((s) => s.login);
   const [control, setControl] = useState(false);
+  const [tasks, setTasks]=useState([]);
   console.log(getCurrentWeek());
   const callback = useCallback(async () => {
     try {
@@ -21,6 +22,7 @@ const Home = (p) => {
       }
       const data = await response.json();
       console.log(data);
+      setTasks(data[0].plans)
     } catch (e) {
       console.error(e);
     }
@@ -32,16 +34,16 @@ const Home = (p) => {
   return (
     <div className={c.container}>
       <ul className={c.underList}>
-        <li
-          style={
-            control === "tlc"
-              ? { opacity: 1, borderBottom: "2px solid white" }
-              : {}
-          }
-          onClick={(e) => setControl("tlc")}
-        >
-          teamLeader by crew
-        </li>
+        {tasks.length>0 && tasks.map(m=><li
+            style={
+              control === m.crew
+                ? { opacity: 1, borderBottom: "2px solid white" }
+                : {}
+            }
+            onClick={(e) => setControl(m.crew)}
+          >
+            {m.crew}
+          </li>) }
       </ul>
     </div>
   );
