755d481d7b57488d558717717af4a418dd63d3e0
anass zeroual <anasszeroual09@gmail.com>
2024-05-08 09:30:56 +0100

    pioli

--- src/components/auditor/Home.js
+++ src/components/auditor/Home.js
@@ -1,23 +1,21 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import c from "./Home.module.css";
 import api from "../../service/api";
 import { useSelector } from "react-redux";
 import { getCurrentWeek } from "../hooks/hfunc";
 const Home = (p) => {
   const { isLoged } = useSelector((s) => s.login);
+  const [control, setControl] = useState(false);
   console.log(getCurrentWeek());
   const callback = useCallback(async () => {
     try {
-      const response = await fetch(
-        `${api}/planning?week=${getCurrentWeek()}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${isLoged.token}`,
-          },
-        }
-      );
+      const response = await fetch(`${api}/planning?week=${getCurrentWeek()}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${isLoged.token}`,
+        },
+      });
       if (!response.ok) {
         throw new Error(response.status);
       }
@@ -31,6 +29,21 @@ const Home = (p) => {
     callback();
   }, [callback]);
 
-  return <div className={c.container}></div>;
+  return (
+    <div className={c.container}>
+      <ul className={c.underList}>
+        <li
+          style={
+            control === "tlc"
+              ? { opacity: 1, borderBottom: "2px solid white" }
+              : {}
+          }
+          onClick={(e) => setControl("tlc")}
+        >
+          teamLeader by crew
+        </li>
+      </ul>
+    </div>
+  );
 };
 export default Home;
