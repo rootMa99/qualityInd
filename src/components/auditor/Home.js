71a96f740edc4804a1e28fd09c19b65b6db256e0
anass zeroual <anasszeroual09@gmail.com>
2024-05-08 09:05:05 +0100

    tea

--- src/components/auditor/Home.js
+++ src/components/auditor/Home.js
@@ -1,7 +1,32 @@
+import { useCallback, useEffect } from "react";
 import c from "./Home.module.css";
-
+import api from "../../service/api";
+import { useSelector } from "react-redux";
 const Home = (p) => {
-    
+  const { isLoged } = useSelector((s) => s.login);
+  
+  const callback = useCallback(async () => {
+    try {
+      const response = await fetch(`${api}/user`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${isLoged.token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  }, [isLoged.token]);
+  useEffect(() => {
+    callback();
+  }, [callback]);
+
   return <div className={c.container}></div>;
 };
 export default Home;
--- src/components/hooks/hfunc.js
+++ src/components/hooks/hfunc.js
@@ -11,3 +11,17 @@ export const getCurrentWeekNumber = () => {
     now.getFullYear() + "-W" + String(correctedWeekNumber).padStart(2, "0")
   );
 };
+export const getCurrentWeek = () => {
+    const now = new Date();
+    const startOfYear = new Date(now.getFullYear(), 0, 0);
+    const diff = now - startOfYear;
+    const oneWeek = 1000 * 60 * 60 * 24 * 7;
+    const weekNumber = Math.floor(diff / oneWeek)+1;
+    const dayOfWeek = now.getDay() || 7;
+    const correctedWeekNumber = dayOfWeek === 1 ? weekNumber + 1 : weekNumber ;
+  
+    return (
+      now.getFullYear() + "-W" + String(correctedWeekNumber).padStart(2, "0")
+    );
+  };
+  
