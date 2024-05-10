7d39f7983354aa69ff033f98c7022d54b335ccaf
anass zeroual <anasszeroual09@gmail.com>
2024-05-10 16:23:28 +0100

    dt

--- src/components/routes/Auditor.js
+++ src/components/routes/Auditor.js
@@ -2,15 +2,15 @@ import { Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "../auditor/Home";
 
-const Auditor=p=>{
-    return (
-        <Suspense>
-          <Routes>
-            <Route index path="/" element={<Navigate replace to="/home" />} />
-            <Route exact path="/home" element={<Home />} />
-            <Route path="*" element={<Navigate replace to="/home" />} />
-          </Routes>
-        </Suspense>
-      );
-}
+const Auditor = (p) => {
+  return (
+    <Suspense>
+      <Routes>
+        <Route index path="/" element={<Navigate replace to="/home" />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route path="*" element={<Navigate replace to="/home" />} />
+      </Routes>
+    </Suspense>
+  );
+};
 export default Auditor;
