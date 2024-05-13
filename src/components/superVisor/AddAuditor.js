ee6cb590cae5d9187d756515540ff2245e3a5e77
anass zeroual <anasszeroual09@gmail.com>
2024-05-13 09:00:58 +0100

    rrt

--- src/components/superVisor/AddAuditor.js
+++ src/components/superVisor/AddAuditor.js
@@ -1,17 +1,22 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import api from "../../service/api";
+import c from "./AddAuditor.module.css";
 const AddAuditor = (p) => {
   const { isLoged } = useSelector((s) => s.login);
+  const [auditors, setAuditors] = useState([]);
   const callback = useCallback(async () => {
     try {
-      const response = await fetch(`${api}/user/?role=Auditor&isBelongTo=false`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${isLoged.token}`,
-        },
-      });
+      const response = await fetch(
+        `${api}/user/?role=Auditor&isBelongTo=false`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${isLoged.token}`,
+          },
+        }
+      );
       if (!response.ok) {
         throw new Error(response.status);
       }
@@ -25,6 +30,19 @@ const AddAuditor = (p) => {
     callback();
   }, [callback]);
 
-  return <div></div>;
+  return (
+    <div className={c.container}>
+      <div>
+        <div>
+          <span>matricule:</span>
+          <span>865</span>
+        </div>
+        <div>
+          <span>fullname:</span>
+          <span>OUBA SAID</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default AddAuditor;
