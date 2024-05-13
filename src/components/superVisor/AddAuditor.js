ca4c772d9c25a5bd6dd6e9a437db02bae86165e2
anass zeroual <anasszeroual09@gmail.com>
2024-05-13 08:52:42 +0100

    tysd

--- src/components/superVisor/AddAuditor.js
+++ src/components/superVisor/AddAuditor.js
@@ -1,4 +1,30 @@
+import { useCallback, useEffect } from "react";
+import { useSelector } from "react-redux";
+import api from "../../service/api";
 const AddAuditor = (p) => {
+  const { isLoged } = useSelector((s) => s.login);
+  const callback = useCallback(async () => {
+    try {
+      const response = await fetch(`${api}/user/?role=Auditor&isBelongTo=false`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${isLoged.token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  }, [isLoged.token]);
+  useEffect(() => {
+    callback();
+  }, [callback]);
+
   return <div></div>;
 };
 export default AddAuditor;
