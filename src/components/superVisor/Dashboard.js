3f145ff3434f0396a932672c455850f5fe55d04f
anass zeroual <anasszeroual09@gmail.com>
2024-05-14 10:00:05 +0100

    785

--- src/components/superVisor/Dashboard.js
+++ src/components/superVisor/Dashboard.js
@@ -1,7 +1,14 @@
 import c from "./Dashboard.module.css";
 
 const Dashboard = (p) => {
-  return <div className={c.container}></div>;
+  return (
+    <div className={c.container}>
+      <div className={c.title}>
+        <div className={c.line}></div>
+        <h1>dashboard</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
