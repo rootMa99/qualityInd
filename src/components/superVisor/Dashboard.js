c9fc5e48e07cd4cb0f9a9f017600c689b7c2c367
anass zeroual <anasszeroual09@gmail.com>
2024-05-14 10:50:25 +0100

    lopmz

--- src/components/superVisor/Dashboard.js
+++ src/components/superVisor/Dashboard.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import c from "./Dashboard.module.css";
 
-
 const Dashboard = (p) => {
-    const [today, setToday]=useState(new Date().toISOString().split("T")[0]);
-    console.log(today)
+  const [today, setToday] = useState(new Date().toISOString().split("T")[0]);
+  console.log(today);
   return (
     <div className={c.container}>
       <div className={c.title}>
@@ -12,8 +11,14 @@ const Dashboard = (p) => {
         <h1>dashboard</h1>
       </div>
       <div className={c.inputD}>
-        <h3>choosen date:</h3>
-        <input type="date" value={today} onChange={e=>setToday(e.target.value)} max={new Date().toISOString().split("T")[0]} pattern="yyyy-mm-dd"/>
+        <h3>select date:</h3>
+        <input
+          type="date"
+          value={today}
+          onChange={(e) => setToday(e.target.value)}
+          max={new Date().toISOString().split("T")[0]}
+          pattern="yyyy-mm-dd"
+        />
       </div>
     </div>
   );
